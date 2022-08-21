67e33b42cf73fc6e59e7917b8026a295eae542a0
zuhaib <mcf2000656@ue.edu.pk>
2022-08-21 19:06:43 +0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
+import store from "./store";
+import "./css/style.css";
 
-import './css/style.css'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App).use(store).use(router).mount("#app");
